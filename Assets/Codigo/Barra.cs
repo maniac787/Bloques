2d5b905eea9c7dee08ba79c8b21174a5750ccac4
maniac787 <maniac787@gmail.com>
2016-04-30 10:32:01 -0500

    Bloques
    Bloques unity

---
+++ Assets/Codigo/Barra.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barra : MonoBehaviour
+{
+    public float Velocidad = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        //-1 , 0 , 1 
+	    float tecladoHorizontal = Input.GetAxisRaw("Horizontal");
+	    float posX = transform.position.x + (tecladoHorizontal * Velocidad * Time.deltaTime);
+
+        transform.position = new Vector3(Mathf.Clamp(posX, -8, 8), transform.position.y); 
+	}
+}
---
+++ Assets/Codigo/Pelota.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pelota : MonoBehaviour {
+
+    bool EnJuego;
+    public Rigidbody RigidPelota;
+
+	// Use this for initialization
+	void Start () {
+        //RigidPelota = GetComponent<Rigidbody>();
+	}
+
+    void Awake()
+    {
+        
+    }
+	// Update is called once per frame
+	void Update () {
+        //Debug.logger.Log(!EnJuego && Input.GetButtonDown("Fire1"));
+        Debug.logger.Log("esta iniciado: "+ EnJuego);
+        Debug.logger.Log("Fire : "+ Input.GetButtonDown("Fire1").ToString());
+        if (!EnJuego && Input.GetButtonDown("Fire1"))
+        {
+            EnJuego = true;
+            transform.SetParent(null);
+            RigidPelota.isKinematic = false;
+            RigidPelota.AddForce(new Vector3(10, 10));
+            RigidPelota.velocity = new Vector3(20, 20);
+        }
+	}
+}
